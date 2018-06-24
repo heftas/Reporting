254c621f6b3df4ab62cc7cd045162a607c7d788b
imed rzigui <[imedrzigui@gmail.com]>
2018-06-25 01:04:00 +0200

    reporting

--- src/ReportingBundle/Controller/DefaultController.php
+++ src/ReportingBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace ReportingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ReportingBundle:Default:index.html.twig');
+        return $this->render('@Reporting/Default/index.html.twig');
     }
 }
