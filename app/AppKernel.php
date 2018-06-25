ae722222ecbe4a12f3c5fb9683ac4ec1bdf32d8a
imed rzigui <[imedrzigui@gmail.com]>
2018-06-26 00:09:10 +0200

    User login

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
             new ReportingBundle\ReportingBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
